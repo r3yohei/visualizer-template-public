44cfd12f615bd9ba3795c25eecd535badb8b052b
r3yohei <r3yohei@gmail.com>
2025-02-11 19:08:27 +0900

    add util.rs, template of lib.rs, edit readme

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,8 +1,9 @@
 use wasm_bindgen::prelude::*;
+mod util;
 
 #[wasm_bindgen]
 pub fn gen(seed: i32) -> String {
-    "".to_string()
+    util::gen(seed as u64).to_string()
 }
 
 #[wasm_bindgen(getter_with_clone)]
@@ -14,14 +15,30 @@ pub struct Ret {
 
 #[wasm_bindgen]
 pub fn vis(_input: String, _output: String, turn: usize) -> Ret {
-    Ret {
-        score: 0,
-        err: "".to_string(),
-        svg: "".to_string(),
+    let input = util::parse_input(&_input);
+    let output_result = util::parse_output(&input, &_output);
+    match output_result {
+        Ok(output) => {
+            let (score, err, svg) = util::vis(&input, &output, turn);
+            Ret {
+                score: score as i64,
+                err: err.to_string(),
+                svg: svg.to_string(),
+            }
+        }
+        Err(err) => Ret {
+            score: 0,
+            err: err.to_string(),
+            svg: String::new(),
+        },
     }
 }
 
 #[wasm_bindgen]
 pub fn get_max_turn(_input: String, _output: String) -> usize {
-    0
+    let input = util::parse_input(&_input);
+    match util::parse_output(&input, &_output) {
+        Ok(out) => out.out.len(),
+        Err(_) => 0,
+    }
 }
